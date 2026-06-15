63676792c86252ecaba7c8979689aad9c35847a8
Alejandro-QV <gabriel.qv1010@gmail.com>
2026-06-15 18:32:40 -0500

    Implementacion algoritmo bt

--- main.py
+++ main.py
@@ -44,3 +44,48 @@ def es_valido(x, y):
 
 print("Movimiento válido:", es_valido(8, 0))
 print("Movimiento válido:", es_valido(0, 4))
+
+def backtracking(x, y, vidas):
+
+    if vidas <= 0:
+        return False
+
+    if (x, y) == fin:
+        solucion[x][y] = 1
+        return True
+
+    visitado[x][y] = True
+    solucion[x][y] = 1
+
+    movimientos = [
+        (1, 0),
+        (0, 1),
+        (-1, 0),
+        (0, -1)
+    ]
+
+    for dx, dy in movimientos:
+
+        nx = x + dx
+        ny = y + dy
+
+        if es_valido(nx, ny):
+
+            nueva_vida = vidas
+
+            if laberinto[nx][ny] == -1:
+                nueva_vida -= 1
+
+            elif laberinto[nx][ny] == -2:
+                nueva_vida -= 2
+
+            if backtracking(nx, ny, nueva_vida):
+                return True
+
+    solucion[x][y] = 0
+
+    return False
+
+resultado = backtracking(inicio[0], inicio[1], 3)
+
+print("Resultado:", resultado)
